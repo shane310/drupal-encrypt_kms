e75f8cbf0fda4f3c0a97d01f11b5d014044b5746
Nick Santamaria <nick.santamaria@previousnext.com.au>
2017-11-16 03:25:25 +1300

    Initial work. Basic configuration, no tests, no fancy exception handling.

---
+++ src/Plugin/EncryptionMethod/AwsKmsEncryptionMethod.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Drupal\encrypt_kms\Plugin\EncryptionMethod;
+
+use Drupal\encrypt\EncryptionMethodInterface;
+use Drupal\encrypt\Exception\EncryptException;
+use Drupal\encrypt\Plugin\EncryptionMethod\EncryptionMethodBase;
+use Drupal\encrypt_kms\KmsClientFactory;
+use Masterminds\HTML5\Exception;
+
+/**
+ * Class AwsKmsEncryptionMethod.
+ *
+ * @EncryptionMethod(
+ *   id = "aws_kms",
+ *   title = @Translation("Amazon KMS"),
+ *   description = "Encryption using Amazon KMS",
+ *   key_type = {"aws_kms"}
+ * )
+ */
+class AwsKmsEncryptionMethod extends EncryptionMethodBase implements EncryptionMethodInterface {
+
+  /**
+   * The settings.
+   *
+   * @var \Drupal\Core\Config\ImmutableConfig
+   */
+  protected $settings;
+
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function checkDependencies($text = NULL, $key = NULL) {
+    $errors = array();
+
+    if (!class_exists('\Aws\Kms\KmsClient')) {
+      $errors[] = $this->t('AWS KMS PHP library is not correctly installed.');
+    }
+
+    $config = \Drupal::config('encrypt_kms');
+
+    // @todo Check AWS credentials exist
+    // Check KMS key ID configured
+
+    return $errors;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function encrypt($text, $key, $options = array()) {
+    $client = \Drupal::service('kms_encrypt.kms_client');
+    try {
+      $result = $client->encrypt([
+        'KeyId' => $key,
+        'Plaintext' => $text,
+      ]);
+
+      return $result['CiphertextBlob'];
+    }
+    catch (Exception $e) {
+      return FALSE;
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function decrypt($text, $key, $options = array()) {
+    $client = \Drupal::service('kms_encrypt.kms_client');
+    try {
+      $result = $client->decrypt([
+        'KeyId' => $key,
+        'CiphertextBlob' => $text,
+      ]);
+
+      return $result['Plaintext'];
+    }
+    catch (Exception $e) {
+      return FALSE;
+    }
+  }
+
+}
